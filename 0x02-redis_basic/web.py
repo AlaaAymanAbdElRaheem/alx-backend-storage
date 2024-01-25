77574cd6f588caa173cd09e74a310f3c415abbc5
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-25 14:58:23 +0200

    fixing task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,14 +5,21 @@ of a particular URL and returns it."""
 
 import requests
 import redis
+from typing import Callable
+from functools import wraps
+
+
+def count(method: Callable) -> Callable:
+    """ counts how many times methods of the Cache class are called"""
+    @wraps(method)
+    def wrapper(url):
+        """ Wrapper function """
+        connection = redis.Redis()
+        connection.incr("count:{}".format(url))
+        result = method(url)
+        connection.setex("result:{}".format(url), 10, result)
 
 
 def get_page(url: str) -> str:
-    """ tracks how many times a particular URL was accessed
-    in the key "count:{url}" and cache the result
-    with an expiration time of 10 seconds"""
-    link = requests.get(url)
-    connection = redis.Redis()
-    connection.incr("count:{}".format(url))
-    connection.setex("result:{}".format(url), 10, link.text)
-    return link.text
+    """ returns the HTML content of the URL """
+    return requests.get(url).text
