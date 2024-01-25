66bdfdc1eb9c656f2863ee168946edb5d2c39b66
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-25 14:42:17 +0200

    task 5

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""uses the requests module to obtain the HTML content
+of a particular URL and returns it."""
+
+
+import requests
+import redis
+
+
+def get_page(url: str) -> str:
+    """ tracks how many times a particular URL was accessed
+    in the key "count:{url}" and cache the result
+    with an expiration time of 10 seconds"""
+    link = requests.get(url)
+    connection = redis.Redis()
+    connection.setex("count:{}".format(url), 10, link.text)
+    connection.incr("count:{}".format(url))
+    return link.text
