d63e91f5a894d35fe40dd00652d23b6a3d6fbddc
AlaaAymanAbdElRaheem <alaa251996@gmail.com>
2024-01-25 15:01:35 +0200

    fixing task 5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,17 +9,18 @@ from typing import Callable
 from functools import wraps
 
 
-connection = redis.Redis()
-
 
 def count(method: Callable) -> Callable:
     """ counts how many times methods of the Cache class are called"""
     @wraps(method)
     def wrapper(url):
         """ Wrapper function """
+        connection = redis.Redis()
         connection.incr(f"count:{url}")
         result = method(url)
         connection.setex(f"result:{url}", 10, result)
+        return result
+    return wrapper
 
 
 def get_page(url: str) -> str:
